3d73d5c7a41d9e22408fbdb0c12b8a7918767770
Aglomation <113240095+Muminwilmer@users.noreply.github.com>
2024-03-08 20:21:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve static files from the 'public' directory at the root
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
