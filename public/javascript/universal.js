f551b8c0be4462260741674fdf5bbbe92ddeaa6e
Aglomation <113240095+Muminwilmer@users.noreply.github.com>
2024-05-02 10:07:32 +0200

    a

--- public/javascript/universal.js
+++ public/javascript/universal.js
@@ -1,15 +1,18 @@
-// function getCookie(cookieName) {
-//   let name = cookieName + "=";
-//   let decodedCookie = decodeURIComponent(document.cookie);
-//   let ca = decodedCookie.split(';');
-//   for (let i = 0; i < ca.length; i++) {
-//     let c = ca[i];
-//     while (c.charAt(0) == ' ') {
-//       c = c.substring(1);
-//     }
-//     if (c.indexOf(name) == 0) {
-//       return c.substring(name.length, c.length);
-//     }
-//   }
-//   return "";
-// }
+if (localStorage.getItem('oldEvent') == null) {
+    localStorage.setItem('oldEvent', parsedID)
+}
+if (localStorage.getItem('onlySelectedCity') == null) {
+    localStorage.setItem('onlySelectedCity', true)
+}
+if (localStorage.getItem('onlySelectedEvent') == null) {
+    localStorage.setItem('onlySelectedEvent', true)
+}
+if (localStorage.getItem('onlySelectedBoth') == null) {
+    localStorage.setItem('onlySelectedBoth', false)
+}
+if (localStorage.getItem('cities') == null) {
+    localStorage.setItem('cities', true)
+}
+if (localStorage.getItem('events') == null) {
+    localStorage.setItem('events', true)
+}
