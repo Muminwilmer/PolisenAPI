524856d6e5799e6013c14f919aa0e0205eefa36d
Aglomation <wilmer.heltborg2008@gmail.com>
2024-11-26 21:26:33 +0100

    a

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-// Serve static files from the 'public' directory at the root
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
